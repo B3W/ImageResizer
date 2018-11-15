d8d90cad6b8bec464acc63338e8d7ad62fcc745e
B3W <westieberg@gmail.com>
2018-11-15 16:15:35 -0600

    Constructor for extracting pixel info from file complete.

--- src/ImageProcessor.java
+++ src/ImageProcessor.java
@@ -11,16 +11,28 @@ import java.io.IOException;
 public class ImageProcessor {
 	
 	/**
-	 * Class representing a pixel in an image
+	 * Class representing a pixel in an image. Pixel's
+	 * identifying info is its RGB format values.
 	 * @author Weston Berg
 	 */
 	private class Pixel {
 		private int r, g, b;
 		
+		/**
+		 * Constructs a new pixel instance
+		 * @param R  Red value
+		 * @param G  Green value
+		 * @param B  Blue value
+		 */
 		public Pixel(int R, int G, int B) {
 			r = R;
 			g = G;
 			b = B;
+		} // Pixel
+		
+		@Override
+		public String toString() {
+			return "[" + r + "," + g + "," + b + "]";
 		}
 	}
 	
@@ -37,7 +49,7 @@ public class ImageProcessor {
 		try (BufferedReader br = new BufferedReader(new FileReader(FName))) {  // Open file for reading
 			String line;
 			String[] spltLine;
-			int i, j;
+			int i, j, r, g, b;
 			
 			if ((line = br.readLine()) != null) {  // Read image height
 				imgH = Integer.parseUnsignedInt(line);
@@ -47,14 +59,40 @@ public class ImageProcessor {
 			}
 			imgMatrix = new Pixel[imgH][imgW];
 			
-			while ((line = br.readLine()) != null) {
-				
+			i = j = 0;
+			while ((line = br.readLine()) != null) {  // Read in pixel info
+				spltLine = line.split(" ");
+				for (int index = 0; index < spltLine.length; index+=3) {
+					r = Integer.parseInt(spltLine[index]);
+					g = Integer.parseInt(spltLine[index+1]);
+					b = Integer.parseInt(spltLine[index+2]);
+					imgMatrix[i][j] = new Pixel(r, g, b);
+					j++;
+				}
+				i++;
+				j = 0;
 			}
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-	}
+	} // ImageProcessor
 	
+	/*
+	 * (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		StringBuilder ipStr = new StringBuilder();
+		for (int i = 0; i < imgH; i++) {
+			for (int j = 0; j < imgW; j++) {
+				ipStr.append(imgMatrix[i][j].toString());
+				ipStr.append(" ");
+			}
+			ipStr.append('\n');
+		}
+		return ipStr.toString();
+	} // toString
 }
